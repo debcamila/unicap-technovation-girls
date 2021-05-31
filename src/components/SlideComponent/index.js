60f1517a3dc472e26f024adc79b15cb4e223d139
debcamila <camiladeborah72@gmail.com>
2021-05-30 21:43:24 -0300

    componentes da seção do slide

---
+++ src/components/SlideComponent/index.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import ImageSlide from '../../static/images/banner.png';
+import "./style.css";
+import { Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+
+export default (function SlideComponents() {
+    return (
+        <div className="slide">
+            <a href="" >
+                <img className="imagem"
+                    src={ImageSlide}
+                    alt="Imagem do slide"
+                    title="Imagem do slide"
+                />
+            </a>
+            <div className="noticias">
+                <Typography className="title-noticias" variant="h6">
+                    Incrições abertas para a Unicap Icam Tech School for Girls
+                </Typography>
+            </div>
+
+            <div className="div-noticias">
+                <Button variant="contained" size="large" className="btn-noticias" >
+                    Notícias
+                </Button>
+            </div>
+
+        </div>
+    )
+})
