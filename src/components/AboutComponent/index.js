72e0a43b1fe883e73edad0fec4e12b156faa06b4
debcamila <camiladeborah72@gmail.com>
2021-06-05 16:52:18 -0300

    estilo para o texto da seção sobre

--- src/components/AboutComponent/index.js
+++ src/components/AboutComponent/index.js
@@ -21,7 +21,7 @@ export default (function AboutComponent() {
                         Em 2021, será realizada a 1ª edição da Unicap technovation for Girls. O objetivo da escola é ensinar meninas de 10 a 18 anos a desenvolverem um protótipo de um aplicativo de celular, partindo de um problema do mundo real que elas desejam solucionar. Durante a jornada, as meninas irão desenvolver várias habilidades como, <strong>resolução de problemas</strong>, <strong>programação</strong>, <strong>empreendedorismo</strong> e <strong>liderança</strong>. No final, espera-se que os projetos desenvolvidos na escola sejam apresentados no Technovation Girls, a maior competição para meninas do mundo inteiro.
                     </Typography>
                     <br></br>
-                    <Typography style={{ textAlign: 'left' }}>
+                    <Typography style={{ textAlign: 'left', color: '#351B69', fontWeight: 'bold' }}>
                         E sabe o mais bacana? É tudo de GRAÇA!
                     </Typography>
 
