9c9c99e6a379186b019c88de4b33e50e9f714b08
debcamila <camiladeborah72@gmail.com>
2021-06-06 00:27:42 -0300

    estrutura e imagens das mentoras

--- src/components/MentoresComponent/index.js
+++ src/components/MentoresComponent/index.js
@@ -1,10 +1,12 @@
 
 import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import logo from '../../static/images/logo.png';
+import mentora1 from '../../static/images/beatriz-ramos.jpeg';
+import mentora2 from '../../static/images/catherine-aussourd.jpeg';
+import mentora3 from '../../static/images/beatriz-ramos.jpeg';
+import mentora4 from '../../static/images/izabella-esther.jpeg';
+import mentora5 from '../../static/images/maria-carmo.jpeg';
+import mentora6 from '../../static/images/marilia-vilar.jpeg';
 import "./style.css";
 
 
@@ -22,91 +24,71 @@ export default (function MentoresComponent() {
 
                 <Grid container spacing={5} style={{marginTop:'30px'}}>
                     <Grid item xs={12} sm={4}>
-                            <Card variant="outlined">
-                                <CardActionArea style={{ height: '100%' }}>
-                                    <CardContent style={{ textAlign: 'center', paddingTop: '20px' }}>
-                                        <img style={{width: '100%', height:'180px'}}
-                                            src={logo}
-                                            alt="Mentora"
-                                            title="Mentora"
-                                        />
-
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
-                        </Grid>
+                        <img style={{width: '200px', height:'200px', borderRadius:'100%'}}
+                            src={mentora1}
+                            alt="Beatriz Ramos"
+                            title="Beatriz Ramos"
+                        />
+                        <Typography className="subtitle" variant="h6">
+                            Beatriz Ramos
+                        </Typography>
+                    </Grid>
 
                         <Grid item xs={12} sm={4}>
-                            <Card variant="outlined">
-                                <CardActionArea style={{ height: '100%' }}>
-                                    <CardContent style={{ textAlign: 'center', paddingTop: '20px' }}>
-                                        <img style={{width: '100%', height:'180px'}}
-                                            src={logo}
-                                            alt="Mentora"
-                                            title="Mentora"
-                                        />
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
+                            <img style={{width: '200px', height:'200px', borderRadius:'100%'}}
+                                src={mentora2}
+                                alt="Catherine Aussourd"
+                                title="Catherine Aussourd"
+                            />    
+                            <Typography className="subtitle" variant="h6">
+                                Catherine Aussourd
+                            </Typography>
                         </Grid>
 
-                        <Grid item xs={12} sm={4}>
-                            <Card variant="outlined">
-                                <CardActionArea style={{ height: '100%' }}>
-                                    <CardContent style={{ textAlign: 'center', paddingTop: '20px' }}>
-                                        <img style={{width: '100%', height:'180px'}}
-                                            src={logo}
-                                            alt="Mentora"
-                                            title="Mentora"
-                                        />
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
+                        <Grid item xs={12} sm={4}> 
+                            <img style={{width: '200px', height:'200px', borderRadius:'100%'}}
+                                src={mentora3}
+                                alt="Hannatty Mendonça"
+                                title="Hannatty Mendonça"
+                            />
+                            <Typography className="subtitle" variant="h6">
+                                Hannatty Mendonça
+                            </Typography>
                         </Grid>
                 </Grid>
 
                 <Grid container spacing={5} style={{marginTop:'30px'}}>
                     <Grid item xs={12} sm={4} style={{marginBottom: '50px'}}>
-                            <Card variant="outlined">
-                                <CardActionArea style={{ height: '100%' }}>
-                                    <CardContent style={{ textAlign: 'center', paddingTop: '20px' }}>
-                                        <img style={{width: '100%', height:'180px'}}
-                                            src={logo}
-                                            alt="Mentora"
-                                            title="Mentora"
-                                        />
-
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
+                            <img style={{width: '200px', height:'200px', borderRadius:'100%'}}
+                                src={mentora4}
+                                alt="Izabella Esther"
+                                title="Izabella Esther"
+                            />
+                            <Typography className="subtitle" variant="h6">
+                                Izabella Esther
+                            </Typography>
                         </Grid>
 
                         <Grid item xs={12} sm={4}>
-                            <Card variant="outlined">
-                                <CardActionArea style={{ height: '100%' }}>
-                                    <CardContent style={{ textAlign: 'center', paddingTop: '20px' }}>
-                                        <img style={{width: '100%', height:'180px'}}
-                                            src={logo}
-                                            alt="Mentora"
-                                            title="Mentora"
-                                        />
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
+                            <img style={{width: '200px', height:'200px', borderRadius:'100%'}}
+                                src={mentora5}
+                                alt="Maria do Carmo"
+                                title="Maria do Carmo"
+                            />
+                            <Typography className="subtitle" variant="h6">
+                                Maria do Carmo
+                            </Typography>
                         </Grid>
 
                         <Grid item xs={12} sm={4}>
-                            <Card variant="outlined">
-                                <CardActionArea style={{ height: '100%' }}>
-                                    <CardContent style={{ textAlign: 'center', paddingTop: '20px' }}>
-                                        <img style={{width: '100%', height:'180px'}}
-                                            src={logo}
-                                            alt="Mentora"
-                                            title="Mentora"
-                                        />
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
+                            <img style={{width: '200px', height:'200px', borderRadius:'100%'}}
+                                src={mentora6}
+                                alt="Marília Vilar"
+                                title="Marília Vilar"
+                            />
+                            <Typography className="subtitle" variant="h6">
+                                Marília Vilar
+                            </Typography>
                         </Grid>
                 </Grid>
             </div>
